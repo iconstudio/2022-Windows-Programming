ceea7f3340b869b991f19d2a265358a157d48a8c
Iconer <yoyofa2@hotmail.com>
2022-03-03 02:30:41 +0900

    Case 2 is updated.

--- WP-Project/Case01/main.cpp
+++ WP-Project/Case01/main.cpp
@@ -7,15 +7,54 @@
 
 using namespace std;
 
+class Matrix {
+public:
+	Matrix(const size_t w, const size_t h, const int value = 0)
+		: sz_w(w), sz_h(h), matrix(new int*[h]) {
+		for (int i = 0; i < h; ++i) {
+			matrix[i] = new int[w];
+			fill(&(matrix[i]), &(matrix[i + w - 1]), 0);
+		}
+	}
+
+private:
+	const size_t sz_w, sz_h;
+	int** matrix = nullptr;
+};
+
 int main() {
+	Matrix matrix{ 5, 4 };
+	char command = 0;
+	bool done = false;
+
 	random_device r_device;
 	uniform_int_distribution<> r_distribution(1, 50);
 	default_random_engine r_engine(r_device());
+	r_distribution(r_engine);
 
-	array<int, 50> datas;
-	iota(datas.begin(), datas.end(), 1);
+	array<int, 50> numbers;
+	iota(numbers.begin(), numbers.end(), 1);
+	shuffle(numbers.begin(), numbers.end(), r_engine);
 
-	shuffle(datas.begin(), datas.end(), r_engine);
+	cin.clear();
 
+	const char blank_modifier[] = { '@', '%', '#', '.' };
+	const auto blk_last = end(blank_modifier);
+	bool blk_did = false;
 
+	while (true) {
+		if (done) break;
+
+		cout << "명령어 입력: ";
+		cin >> command;
+
+		command = toupper(command);
+
+		switch (command) {
+			case 'L':
+			{
+			}
+			break
+		}
+	}
 }

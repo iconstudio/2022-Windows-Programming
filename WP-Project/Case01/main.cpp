2fb05158a2d2bc32b6802ca9bb081e969c49910f
Iconer <yoyofa2@hotmail.com>
2022-03-02 21:42:23 +0900

    Case 1 is added.

---
+++ WP-Project/Case01/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <random>
+#include <array>
+#include <numeric>
+
+using namespace std;
+
+int main() {
+	random_device r_device;
+	uniform_int_distribution<> r_distribution(1, 50);
+	default_random_engine r_engine(r_device());
+
+	array<int, 50> datas;
+	iota(datas.begin(), datas.end(), 1);
+
+	generate(datas.begin(), datas.end(), [&]() { return r_distribution(r_engine); });
+
+
+}
